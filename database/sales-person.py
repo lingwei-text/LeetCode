06c1b20871c419f21993b581669242adf090a048
lingwei <84201972+lingwei-text@users.noreply.github.com>
2025-10-03 14:55:48 +0100

    LC-sales-person [Runtime: N/A; Memory: N/A]

---
+++ database/sales-person.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+def sales_person(sales_person: pd.DataFrame, company: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFrame:
+    merge1 = sales_person.merge(orders, how = 'left', on = 'sales_id')
+    merge2 = merge1.merge(company, how = 'left', on = 'com_id')
+    red
+    red_sales = merge2[merge2['name_y']=='RED'][['name_x']]
+    filtered_sales = merge2[['name_x']]
+
+    
