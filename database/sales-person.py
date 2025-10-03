2b535913d669d8fa1d4c01cb264e68893fc29076
lingwei <84201972+lingwei-text@users.noreply.github.com>
2025-10-03 15:00:01 +0100

    LC-sales-person [Runtime: N/A; Memory: N/A]

--- database/sales-person.py
+++ database/sales-person.py
@@ -3,8 +3,8 @@ import pandas as pd
 def sales_person(sales_person: pd.DataFrame, company: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFrame:
     merge1 = sales_person.merge(orders, how = 'left', on = 'sales_id')
     merge2 = merge1.merge(company, how = 'left', on = 'com_id')
-    red
     red_sales = merge2[merge2['name_y']=='RED'][['name_x']]
-    filtered_sales = merge2[['name_x']]
+    filtered_sales = merge2[~merge2['name_x'].isin(red_sales)][['name_x']]
+    return filtered_sales
 
     
