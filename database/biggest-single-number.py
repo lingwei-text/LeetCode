414a08acba9097cf2d72cf15b96e65994ddc1dc0
lingwei <84201972+lingwei-text@users.noreply.github.com>
2025-10-08 12:44:10 +0100

    LC-biggest-single-number [Runtime: N/A; Memory: N/A]

--- database/biggest-single-number.py
+++ database/biggest-single-number.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
 def biggest_single_number(my_numbers: pd.DataFrame) -> pd.DataFrame:
-    my_numbers[['count']] = my_numbers.groupby('num', as_index = False)[['num']].count()
-    filtered_
+    my_numbers = my_numbers.groupby('num', as_index = False).size()
+    return my_numbers
     
