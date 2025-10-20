a65941bd3ad7990bf6e3c1c89f431a9063d6770c
lingwei <84201972+lingwei-text@users.noreply.github.com>
2025-10-20 09:06:47 +0100

    LC-group-sold-products-by-the-date [Runtime: 335 ms; Memory: 68.4 MB]

--- database/group-sold-products-by-the-date.py
+++ database/group-sold-products-by-the-date.py
@@ -1,14 +1,10 @@
 import pandas as pd
 
 def categorize_products(activities: pd.DataFrame) -> pd.DataFrame:
-    import pandas as pd
+    activities = activities.groupby('sell_date', as_index = False).agg(
+        num_sold = ('product','nunique'),
+        products = ('product', lambda x: ','.join(sorted(set(x))))
+    )
+    return activities
 
-def categorize_products(activities: pd.DataFrame) -> pd.DataFrame:
-  groups = activities.groupby('sell_date')
-
-  stats = groups.agg(
-    num_sold=('product', 'nunique'),
-    products=('product', lambda x: ','.join(sorted(set(x))))
-  ).reset_index()
-  stats.sort_values('sell_date', inplace=True)
-  return stats
+    
