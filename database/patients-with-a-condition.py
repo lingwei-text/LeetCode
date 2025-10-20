7590fdaf7f1ab1016ed9ef0e570965cf98811a69
lingwei <84201972+lingwei-text@users.noreply.github.com>
2025-10-20 10:34:49 +0100

    LC-patients-with-a-condition [Runtime: N/A; Memory: N/A]

---
+++ database/patients-with-a-condition.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+def find_patients(patients: pd.DataFrame) -> pd.DataFrame:
+    pattern = r'DIAB1'
+    patients['boo'] = patients['conditions'].str.contains(pattern, na = False)
+    return patients[patients['boo']==True][['patient_id', 'patient_name', 'conditions']]
+
+    
